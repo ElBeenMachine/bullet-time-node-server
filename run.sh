4a777c03439c4e708021cc98035b2230e62d1db4
Ollie Beenham <ollie@beenhamow.co.uk>
2023-10-19 21:24:00 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 echo "Installing Python"
-sudo apt-get install python3
+sudo apt-get install python3 -y
 
 echo "Installing Pip"
-sudo apt-get install python3-pip
+sudo apt-get install python3-pip -y
 
 echo "Installing necessary python modules"
 pip install -r requirements.txt
 
 echo ""
 echo "Installing necessary camera modules"
-sudo apt install -y python3-picamera2
+sudo apt install python3-picamera2 -y
 
 echo ""
 echo "Running Server"
