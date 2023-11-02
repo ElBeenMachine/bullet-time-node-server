635604f930eb06acffdcdc80b53d14f943e6029b
Ollie Beenham <ollie@beenhamow.co.uk>
2023-11-02 17:09:02 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,22 @@ import platform
 
 # Set up the camera
 from picamera2 import Picamera2
+
 cam = Picamera2()
-camera_config = cam.create_preview_configuration(main={"size": (4608, 2592)})
+camera_config = cam.create_preview_configuration(main={"size": (1920, 1080)})
 cam.configure(camera_config)
 cam.start()
 
 app = Flask(__name__)
+
 
 @app.route("/capture")
 def hello():
     # Capture a picture from the source and process it into a Base64 String
     try:
         cam.capture_file("img.jpg")
-        
-        with open('img.jpg', 'rb') as image_file:
+
+        with open("img.jpg", "rb") as image_file:
             data = base64.b64encode(image_file.read())
     except Exception as e:
         print(e)
