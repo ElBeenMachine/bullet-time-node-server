af82f6b7529cb2bb18f550ad281f431c944747e3
Ollie Beenham <ollie@beenhamow.co.uk>
2023-11-06 14:14:29 +0000

    Added scripts

--- update.sh
+++ update.sh
@@ -1,4 +1,10 @@
 echo "Updating Repository"
+
+echo "Disabling running service"
+sudo systemctl start bulletTime.service
+sudo systemctl disable bulletTime.service
+
+echo "Pulling latest production build from GitHub"
 git pull
 
 echo "Running install script"
