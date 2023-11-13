fc149acdec6da9145ae303003dc1c20d4566da16
Ollie Beenham <ollie@beenhamow.co.uk>
2023-11-13 14:06:15 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,7 +5,10 @@ sudo systemctl start bulletTime.service
 sudo systemctl disable bulletTime.service
 
 echo "Pulling latest production build from GitHub"
+git stash
+git stash drop
 git pull
 
 echo "Running install script"
+sudo chmod +x ./install.sh
 ./install.sh
