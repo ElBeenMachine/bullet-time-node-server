339c5d352d2f205ec26b00e66be21fee8266535e
Ollie Beenham <ollie@beenhamow.co.uk>
2023-12-08 16:22:29 +0000

    Crypto update

--- main.py
+++ main.py
@@ -4,51 +4,57 @@ import platform
 from aiohttp import web
 import ssl
 from utils import *
-from cryptography import x509
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives import serialization
 
 
 VERSION = "1.0.5"
 
-# Function to generate a self-signed SSL certificate and key
-def generate_self_signed_cert():
-    key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-        backend=default_backend()
-    )
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import hashes
+from datetime import datetime, timedelta
 
-    subject = issuer = Name([
-        x509.NameAttribute(x509.NameOID.COMMON_NAME, u'localhost')
-    ])
+# Generate a private key
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+    backend=default_backend()
+)
 
-    cert = x509.CertificateBuilder().subject_name(
-        subject
-    ).issuer_name(
-        issuer
-    ).public_key(
-        key.public_key()
-    ).serial_number(
-        x509.random_serial_number()
-    ).not_valid_before(
-        datetime.utcnow()
-    ).not_valid_after(
-        datetime.utcnow() + timedelta(days=365)
-    ).sign(
-        key, default_backend()
-    )
+# Create a self-signed certificate
+subject = issuer = x509.Name([
+    x509.NameAttribute(x509.NameOID.COMMON_NAME, u'localhost')
+])
 
-    cert_pem = cert.public_bytes(encoding=serialization.Encoding.PEM)
-    key_pem = key.private_bytes(
+cert = x509.CertificateBuilder().subject_name(
+    subject
+).issuer_name(
+    issuer
+).public_key(
+    private_key.public_key()
+).serial_number(
+    x509.random_serial_number()
+).not_valid_before(
+    datetime.utcnow()
+).not_valid_after(
+    datetime.utcnow() + timedelta(days=365)
+).add_extension(
+    x509.SubjectAlternativeName([x509.DNSName(u'localhost')]),
+    critical=False,
+).sign(private_key, hashes.SHA256(), default_backend())
+
+# Save private key to a file
+with open('private_key.pem', 'wb') as f:
+    f.write(private_key.private_bytes(
         encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.PKCS8,
+        format=serialization.PrivateFormat.TraditionalOpenSSL,
         encryption_algorithm=serialization.NoEncryption()
-    )
+    ))
 
-    return cert_pem, key_pem
+# Save certificate to a file
+with open('certificate.pem', 'wb') as f:
+    f.write(cert.public_bytes(serialization.Encoding.PEM))
 
 # Create a new Socket.IO server with specified port
 sio = socketio.AsyncServer(cors_allowed_origins='*', logger=True)
@@ -78,12 +84,9 @@ def event_error(sid, error):
 if __name__ == '__main__':
     port = 8080
 
-    # Generate self-signed SSL certificate and key
-    cert_pem, key_pem = generate_self_signed_cert()
-
     # Create an SSL context
     ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-    ssl_context.load_cert_chain(certfile=cert_pem, keyfile=key_pem)
+    ssl_context.load_cert_chain(certfile="certificate.pem", keyfile="private_key.pem")
 
     # Set up the Socket.IO server over HTTPS
     sio.attach(app, ssl_context=ssl_context)
