01cd02900ffd91fed0fb1dac3490b5945eb18662
Ollie Beenham <ollie@beenhamow.co.uk>
2023-12-08 16:12:33 +0000

    Added crypto

--- main.py
+++ main.py
@@ -2,9 +2,51 @@
 import socketio
 import platform
 from aiohttp import web
+import ssl
 from utils import *
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.x509 import Name, CertificateBuilder, load_pem_x509_certificate
 
 VERSION = "1.0.5"
+
+# Function to generate a self-signed SSL certificate and key
+def generate_self_signed_cert():
+    key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+
+    subject = issuer = Name([
+        x509.NameAttribute(NameOID.COMMON_NAME, u'localhost')
+    ])
+
+    cert = CertificateBuilder().subject_name(
+        subject
+    ).issuer_name(
+        issuer
+    ).public_key(
+        key.public_key()
+    ).serial_number(
+        x509.random_serial_number()
+    ).not_valid_before(
+        datetime.utcnow()
+    ).not_valid_after(
+        datetime.utcnow() + timedelta(days=365)
+    ).sign(
+        key, default_backend()
+    )
+
+    cert_pem = cert.public_bytes(encoding=serialization.Encoding.PEM)
+    key_pem = key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+
+    return cert_pem, key_pem
 
 # Create a new Socket.IO server with specified port
 sio = socketio.AsyncServer(cors_allowed_origins='*', logger=True)
@@ -33,4 +75,15 @@ def event_error(sid, error):
 # Set the port for the Socket.IO server
 if __name__ == '__main__':
     port = 8080
+
+    # Generate self-signed SSL certificate and key
+    cert_pem, key_pem = generate_self_signed_cert()
+
+    # Create an SSL context
+    ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    ssl_context.load_cert_chain(certfile=cert_pem, keyfile=key_pem)
+
+    # Set up the Socket.IO server over HTTPS
+    sio.attach(app, ssl_context=ssl_context)
+
     web.run_app(app, port=port)
