73bbcca2455e64d7235b339ddb5aaedbd0239fbf
Ollie Beenham <ollie@beenhamow.co.uk>
2023-12-11 19:14:26 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from utils import *
 import os
 import subprocess
 
-VERSION = "1.4.0"
+VERSION = "1.5.0"
 
 # Create a new Socket.IO server with specified port
 sio = socketio.AsyncServer(cors_allowed_origins='*')
