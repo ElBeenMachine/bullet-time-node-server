d52467c86f50bc106b5e73e10bd152b85da62093
Ollie Beenham <ollie@beenhamow.co.uk>
2023-12-21 12:11:05 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -38,7 +38,7 @@ def captureFrame(stream):
     cam.start() 
 
     # Capture frame into stream
-    cam.capture_buffer(stream, format='jpeg', use_video_port=True)
+    cam.capture_buffer(stream, use_video_port=True)
 
     # Load image from stream
     frameData = stream.read()
