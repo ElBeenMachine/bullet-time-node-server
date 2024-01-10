299a13fa437958fca1ec7b0a9ae317cf2e3ff694
Ollie Beenham <ollie@beenhamow.co.uk>
2024-01-10 16:43:48 +0000

    Updated version number

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Import libraries
 from utils import *
 
-VERSION = "2.0.4"
+VERSION = "2.0.5"
 
 # Create a new Socket.IO server with specified port
 sio = socketio.AsyncServer(cors_allowed_origins='*')
